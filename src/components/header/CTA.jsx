67f18bab4190802d524ee3266a623f964ef78452
Arjun Karthik <77539979+arjunkarthik123@users.noreply.github.com>
2022-10-18 01:25:49 -0400

    update resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import resume from '../../assets/Resume Arjun Karthik - 2022 (3).pdf'
+import resume from '../../assets/Resume Arjun Karthik - 2022 (4).pdf'
 
 const CTA = () => {
   return (
